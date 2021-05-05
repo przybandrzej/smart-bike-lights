28a7223ed87e15871478cdefbdc840d7eb0c125d
Andrzej Przybysz <przybysz@trui.pl>
2021-05-05 22:45:57 +0200

    change control algorithm

--- biker-light/biker-light.ino
+++ biker-light/biker-light.ino
@@ -3,12 +3,16 @@
 #include <Adafruit_Sensor.h>
 #include <Adafruit_ADXL345_U.h>
 
+/*
+ * Pin 12 is connected to the DATA IN-pin of the first MAX7221
+ * Pin 11 is connected to the CLK-pin of the first MAX7221
+ * Pin 10 is connected to the LOAD(/CS)-pin of the first MAX7221 
+ * 1 MAX7221 attached to the arduino
+ */
 LedControl lc = LedControl(12, 11, 10, 1);
 Adafruit_ADXL345_Unified accel = Adafruit_ADXL345_Unified(12345);
 
 double xError = 0;
-double yError = -9.81;
-double zError = 1.0;
 double sensorResolution = 0;
 int switch_pin = 7;
 
@@ -19,15 +23,15 @@ enum Side {
 bool isOn = false;
 int loopIterator = 0;
 bool light = false;
-bool value = false;
-Side sideOn = Side.LEFT;
+int val = LOW;
+Side sideOn = LEFT;
 
 void setup() {
   lc.shutdown(0, false);
   lc.setIntensity(0, 8);
   lc.clearDisplay(0);
   Serial.begin(9600);
-  accel.begin()
+  accel.begin();
   accel.setRange(ADXL345_RANGE_2_G);
   sensor_t sensor;
   accel.getSensor(&sensor);
@@ -38,27 +42,50 @@ void setup() {
 void loop() {
   sensors_event_t event;
   accel.getEvent(&event);
-
-  /* Display the results (acceleration is measured in m/s^2) */
   double acc = event.acceleration.x;
-  //Serial.print("Read X "); Serial.print(acc); Serial.println("m/s^2 ");
-  acc = acc - xError;
   if (acc < 0) {
     acc = acc + sensorResolution;
+    acc = acc + xError;
   } else if (acc > 0 ) {
     acc = acc - sensorResolution;
+    acc = acc - xError;
   }
-  /*Serial.print("Sensor resolution "); Serial.println(sensorResolution);
-  Serial.print("X: "); Serial.print(acc); Serial.print("  "); Serial.println("m/s^2 ");
-  double vx = velocity0 + acc * 0.25;
-  Serial.print("X velocity: "); Serial.print(vx);  Serial.println("m/s "); Serial.println("");*/
-
   if (loopIterator == 8) {
     loopIterator = 0;
     light = false;
-    Serial.print("Turn "); Serial.print(sideOn); Serial.println(" light off.");
+    Serial.print("Turn "); Serial.println(" light off.");
   }
-  value = digitalRead(switch_pin); // check mercury switch state
+  val = digitalRead(switch_pin); // check mercury switch state
+  if (val == HIGH) {
+    if (light) {
+      if (sideOn == LEFT && acc < 0) {
+        sideOn = RIGHT;
+        light = true;
+        loopIterator = -1;
+        Serial.print("Turn "); Serial.print(sideOn); Serial.println(" light on.");
+        turnOffTurnLight(LEFT);
+        turnOffTurnLight(RIGHT);
+      } else if (sideOn == RIGHT && acc >= 0) {
+        sideOn = LEFT;
+        light = true;
+        loopIterator = -1;
+        Serial.print("Turn "); Serial.print(sideOn); Serial.println(" light on.");
+        turnOffTurnLight(LEFT);
+        turnOffTurnLight(RIGHT);
+      }
+    } else {
+      if (acc < 0) {
+        sideOn = RIGHT;
+      } else {
+        sideOn = LEFT;
+      }
+      light = true;
+      loopIterator = -1;
+      Serial.print("Turn "); Serial.print(sideOn); Serial.println(" light on.");
+      turnOffTurnLight(LEFT);
+      turnOffTurnLight(RIGHT);
+    }
+  }
   if (light) {
     if (isOn) {
       turnOffTurnLight(sideOn);
@@ -68,21 +95,11 @@ void loop() {
       isOn = true;
     }
   } else {
-    if (value) {
-      if (acc < 0) {
-        sideOn = RIGHT;
-      } else {
-        sideOn = LEFT;
-      }
-      light = true;
-      loopIterator = -1;
-      Serial.print("Turn "); Serial.print(sideOn); Serial.println(" light on.");
-    }
     turnOffTurnLight(LEFT);
     turnOffTurnLight(RIGHT);
   }
   loopIterator++;
-  delay(250);
+  delay(500);
 }
 
 void turnLight(Side side) {
